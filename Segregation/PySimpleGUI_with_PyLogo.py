8b4406f7a373809e70f2f3a8718f0567fa674ad6
Russ Abbott <Russ.Abbott@gmail.com>
2020-01-01 19:05:26 -0800

    Capture keyboard input

--- Segregation/PySimpleGUI_with_PyLogo.py
+++ Segregation/PySimpleGUI_with_PyLogo.py
@@ -28,10 +28,21 @@ class SimpleGUI:
         self.DISPLAY_SHAPE = (screen_pixel_width, screen_pixel_height)
         self.LOWER_LEFT_PIXEL = (self.DISPLAY_SHAPE[1]-1, 0)
         self.UPPER_RIGHT_PIXEL = (0, self.DISPLAY_SHAPE[0]-1)
-        self.STOP = 'Stop'
-        self.SETUP = 'setup'
-        self.GO_ONCE = 'go once'
+
+        self.CTRL_D = 'D:68'
+        self.CTRL_d = 'd:68'
+        self.ESCAPE = 'Escape:27'
+        self.EXIT = 'Exit'
+        self.FPS = 'FPS'
         self.GO = 'go'
+        self.GO_ONCE = 'go once'
+        self.NORMAL = 'normal'
+        self.Q = 'Q'
+        self.q = 'q'
+        self.SETUP = 'setup'
+        self.STOP = 'Stop'
+
+        self.idle_fps = 10
 
         layout = [     # [sg.Text('Test of PySimpleGUI with PyGame')],
                        # [sg.Graph(screen-shape, (0, 0), shape,
@@ -45,9 +56,9 @@ class SimpleGUI:
 
                       [sg.Button(self.SETUP), sg.Button(self.GO_ONCE), sg.Button(self.GO, pad=((0, 50), (0, 0))),
                        sg.Button(self.STOP, button_color=('white', 'darkblue')),
-                       sg.Exit(button_color=('white', 'firebrick4'), key='Exit'),
-                       sg.Text('FPS', pad=((100, 10), (0, 0))),
-                       sg.Slider(key='fps', range=(1, 100), orientation='horizontal',
+                       sg.Exit(button_color=('white', 'firebrick4'), key=self.EXIT),
+                       sg.Text(self.FPS, pad=((100, 10), (0, 0))),
+                       sg.Slider(key=self.FPS, range=(1, 100), orientation='horizontal',
                                  default_value=default_fps, pad=((0, 0), (0, 20)))]
                  ]
 
@@ -63,35 +74,23 @@ class SimpleGUI:
         pg.init()
         self.screen = pg.display.set_mode(self.DISPLAY_SHAPE)
         se.SimEngine.SCREEN_RECT = self.screen.get_rect()
-        self.fps = default_fps
         self.default_fps = default_fps
-
-
-    @staticmethod
-    def exit() -> bool:
-        """
-        Not work. Not getting keyboard events.
-        """
-        for ev in pg.event.get( ):
-            if ev.type == pg.QUIT or \
-                ev.type == pg.KEYDOWN and (ev.key == pg.K_ESCAPE or
-                                           ev.key == pg.K_q or
-                                           # The following tests for ctrl-d.
-                                           # (See https://www.pygame.org/docs/ref/key.html)
-                                           ev.key == pg.K_d and ev.mod & pg.KMOD_CTRL):
-                return True
-        return False
+        self.fps = default_fps
 
     def idle_loop(self):
         simEngine = se.SimEngine.SIM_ENGINE
 
         # Not getting any keyboard events. Always returns False.
-        while not self.exit():
-
+        pg.event.set_grab(False)
+        event = None
+        while event not in [self.ESCAPE, self.q, self.Q, self.CTRL_D, self.CTRL_d]:
             (event, values) = self.window.read(timeout=10)
-            self.fps = values['fps']
+            # if event != '__TIMEOUT__':
+            #     print(f'{type(event)}: {event}')
 
-            if event in (None, 'Exit'):
+            self.fps = values[self.FPS]
+
+            if event in (None, self.EXIT):
                 self.window.close()
                 break
 
@@ -104,27 +103,21 @@ class SimpleGUI:
                 simEngine.draw(self.screen)
 
             if event == self.GO:
-                # saved_fps = self.fps
                 returned_value = self.run_model()
-                # values['fps'] = self.default_fps
-                if returned_value == 'Exit':
+                if returned_value == self.EXIT:
                     self.window.close( )
                     break
 
-            simEngine.clock.tick(10)
-
-    def read_values(self):
-        (_, values) = self.window.read(timeout=10)
-        return values
+            simEngine.clock.tick(self.idle_fps)
 
     def run_model(self):
         simEngine = se.SimEngine.SIM_ENGINE
         while True:
             (event, values) = self.window.read(timeout=10)
-            self.fps = values['fps']
+            self.fps = values[self.FPS]
 
-            if event in (None, 'Exit'):
-                return 'Exit'
+            if event in (None, self.EXIT):
+                return self.EXIT
             if event == self.STOP or se.SimEngine.WORLD.done():
                 break
             simEngine.ticks += 1
@@ -133,4 +126,4 @@ class SimpleGUI:
             simEngine.clock.tick(self.fps)
 
         se.SimEngine.WORLD.final_thoughts()
-        return 'Normal'
+        return self.NORMAL
