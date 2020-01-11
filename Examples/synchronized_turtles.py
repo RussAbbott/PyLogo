02ddb26ffe7b1beed07734c7a9a89701ccea159f
Russ Abbott <Russ.Abbott@gmail.com>
2020-01-10 22:38:56 -0800

    Fully working version

--- Examples/synchronized_turtles.py
+++ Examples/synchronized_turtles.py
@@ -1,144 +1,91 @@
 
-
-"""
-    turtles-own [speed]
-
-    ;breed [circle-drawers cirdle-drawer]
-
-    globals [turtle-size outer]
-
-    to startup
-      setup
-    end
-
-    to setup
-      clear-all
-      set turtle-size 5 ;; easier to see
-      create-ordered-turtles number-of-turtles [
-        set size turtle-size
-        set speed 1
-      ]
-    ;  ask white-turtle [set color white]
-      set outer 50
-    ;  draw-circle outer white
-      reset-ticks
-    end
-
-    ;to draw-circle [radius circle-color]
-    ;  ask patches [if distancexy 0 0 >= 50 [set pcolor white]]
-    ;  ;; Drawing this circle smoothes out the edges.
-    ;  create-ordered-circle-drawers 360 [
-    ;    set speed 0.35  ;; this is the size of each step the turtles take in a tick
-    ;    fd radius  ;; move turtles to perimeter of circle
-    ;    rt 90  ;; turtles face tangent to the circle
-    ;    set color circle-color
-    ;    set pen-size 10
-    ;    pen-down
-    ;    fd 2 * pi * radius / 360  ;; Each circle-drawer moves 1/360 around the circumference
-    ;    die
-    ;  ]
-    ;end
-
-    to go
-      ask turtles [fd speed]
-      let dist-turtle-0-to-0-0 [distancexy 0 0] of turtle 0
-      if dist-turtle-0-to-0-0 > outer - turtle-size / 2 [ask turtles [ facexy 0 0 fd 1 ]]
-      if random 100 < 15  [ask turtles [rt 25]]
-      if random 100 < 15  [ask turtles [lt 25]]
-      tick
-    end
-
-    to toggle-turtle-sizes
-      if [size] of white-turtle = turtle-size [
-        ask turtles [set size turtle-size / 2]
-        ask white-turtle [set size turtle-size * 1.5]
-        stop
-      ]
-      ask turtles [set size turtle-size]
-    end
-
-    to-report white-turtle
-      report turtle (count turtles - 1)
-    end
-"""
-
 import PyLogo.core.core_elements as core
 from PyLogo.core.sim_engine import PyLogo, SimEngine
 import PyLogo.core.utils as utils
 
 from math import pi
 
-from random import random
+from random import randint, random
 
 
-class DrunkenTurtle_World(core.World):
+class Synchronized_Turtle_World(core.World):
 
-    def __init__(self, patch_class=core.Patch, turtle_class=core.Turtle):
-        super().__init__(patch_class=patch_class, turtle_class=turtle_class)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         self.reference_turtle = None
-        self.current_figure_counter = 0
-        self.current_figure = 'random'
+        self.current_figure = None
+        self.breathing_phase = 'inhale'
+
+    def do_one_step(self):
+        if self.current_figure in ['clockwise', 'counter-clockwise']:
+            r = self.reference_turtle.distance_to_xy(utils.CENTER_PIXEL())
+            self.go_in_circle(r)
+        elif self.current_figure == 'breathe':
+            self.breathe()
+        elif self.current_figure == 'random':
+            self.go_randomly()
+        else:
+            print(f'Error. No current figure: ({self.current_figure})')
+
+    def breathe(self):
+        for turtle in self.turtles:
+            if self.breathing_phase == 'inhale':
+                turtle.turn_left(180)
+            turtle.forward()
+
+    def go_in_circle(self, r):
+        for turtle in self.turtles:
+            turtle.turn_left(90 if self.current_figure == 'clockwise' else -90)
+            turtle.forward(2 * pi * r / 360)
+
+    def go_randomly(self):
+        for turtle in self.turtles:
+            turtle.heading = turtle.cached_heading
+            turtle.forward()
+
+        if random() < 0.05:
+            angle = randint(-180, 180)
+            for turtle in self.turtles:
+                turtle.turn_right(angle)
+
+        for turtle in self.turtles:
+            turtle.cached_heading = turtle.heading
+
+    def grow_shrink(self, grow_or_shrink):
+        for turtle in self.turtles:
+            if grow_or_shrink == 'grow':
+                turtle.turn_right(180)
+            turtle.forward()
+            # Set the breathing phase whether or not we are currently breathing.
+            self.breathing_phase = 'inhale' if grow_or_shrink == 'grow' else 'exhale'
 
     def setup(self):
-        nbr_turtles = int(self.get_gui_value('nbr_turtles'))
+        nbr_turtles = self.get_gui_value('nbr_turtles')
         self.create_ordered_turtles(nbr_turtles)
         self.reference_turtle = list(self.turtles)[0]
         for turtle in self.turtles:
+            turtle.cached_heading = turtle.heading
             turtle.speed = 1
             turtle.forward(100)
             turtle.turn_right(90)
 
-    def go_in_circle(self, r):
-        for turtle in self.turtles:
-            turtle.forward(2 * pi * r / 360)
-            turtle.turn_right(1)
-
-    def go_randomly(self):
-        for turtle in self.turtles:
-            turtle.forward()
-
-        if random() < 0.15:
-            for turtle in self.turtles:
-                turtle.turn_right(25)
-        elif random() < 0.15:
-            for turtle in self.turtles:
-                turtle.turn_left(25)
-
-    def grow_shrink(self):
-        for turtle in self.turtles:
-            turtle.face_xy(utils.CENTER_PIXEL())
-            if self.current_figure == 'grow':
-                turtle.turn_right(180)
-            turtle.forward()
-
-    def get_figure(self):
-        self.current_figure = self.get_gui_value('figure')
-        self.current_figure_counter = 100
-
     def step(self):
-        if self.reference_turtle.distance_to_xy(utils.CENTER_PIXEL()) >= 250 and self.current_figure != 'shrink':
-            self.current_figure = 'shrink'
-            self.current_figure_counter = 1
-
-        elif self.reference_turtle.distance_to_xy(utils.CENTER_PIXEL()) <= 50 and self.current_figure != 'grow':
-            self.current_figure = 'grow'
-            self.current_figure_counter = 1
-
-        elif self.current_figure_counter == 0:
-            self.get_figure()
-
-        else:
-            self.current_figure_counter -= 1
-
-        if self.current_figure == 'circle':
-            r = 100
-            self.go_in_circle(r)
-        elif self.current_figure == 'random':
-            self.go_randomly( )
-        elif self.current_figure == 'grow' or self.current_figure == 'shrink':
-            self.grow_shrink()
-        else:
-            print(f'Error no figure: ({self.current_figure}, {self.current_figure_counter})')
+        # For simplicity, start each step by facing the center.
+        for turtle in self.turtles:
+            turtle.face_xy(utils.CENTER_PIXEL( ))
+        if self.take_emergency_action():
+            return
+        self.current_figure = self.get_gui_value('figure')
+        self.do_one_step()
+        
+    def take_emergency_action(self):
+        if self.reference_turtle.distance_to_xy(utils.CENTER_PIXEL()) >= 250:
+            self.grow_shrink('shrink')
+            return True
+        elif self.reference_turtle.distance_to_xy(utils.CENTER_PIXEL()) <= 50:
+            self.grow_shrink('grow')
+            return True
+        return False
 
 
 # ############################################## Define GUI ############################################## #
@@ -148,9 +95,10 @@ gui_elements = [[sg.Text('nbr of turtles'),
                            orientation='horizontal', pad=((0, 0), (0, 20)))],
 
                 [sg.Text('Figure to trace'),
-                 sg.Combo(['circle', 'random', 'square'], key='figure', default_value='random')]
+                 sg.Combo(['breathe', 'clockwise', 'counter-clockwise', 'random'], key='figure',
+                          default_value='clockwise')]
                 ]
 
 if __name__ == "__main__":
     from PyLogo.core.sim_engine import PyLogo
-    PyLogo(DrunkenTurtle_World, gui_elements, 'Synchronized drunken turtles', bounce=None)
+    PyLogo(Synchronized_Turtle_World, gui_elements, 'Synchronized turtles', bounce=None)
