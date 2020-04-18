1d3a6af6ea1f2c1c149e8575cf65e797d3a76782
Russ Abbott <Russ.Abbott@gmail.com>
2020-04-17 19:44:12 -0700

    Improve output. Trim new individuals.

--- Examples/ga_examples/ga_knapsack.py
+++ Examples/ga_examples/ga_knapsack.py
@@ -1,5 +1,6 @@
 
-from random import choice, randint
+from math import floor
+from random import choice, randint, shuffle
 from typing import List
 
 from core.ga import Chromosome, GA_World, Individual, gui_left_upper
@@ -27,20 +28,24 @@ class Knapsack_Chromosome(Chromosome):
     """
 
     def chromosome_fitness(self) -> float:
-        (max_weight, items) = Knapsack_World.problem
-        len_chrom = len(self)
-        total_value = sum([self[i] * items[i].value for i in range(len_chrom)])
-        total_weight = sum([self[i] * items[i].weight for i in range(len_chrom)])
-        return 0 if total_weight > max_weight else total_value
+        (_max_weight, items) = Knapsack_World.problem
+        total_value = sum([self[i] * items[i].value for i in range(len(self))])
+        return total_value
 
 
 class Knapsack_Individual(Individual):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.total_weight = None
+        self.trim_chromosome()
+        self.fitness = self.compute_fitness()
 
     def __eq__(self, other):
         return self.chromosome == other.chromosome
 
     def __str__(self):
-        return f'{self.fitness}<-{"".join([str(gene) for gene in self.chromosome])}'
+        return f'{self.fitness}/{self.total_weight}<-{"".join([str(gene) for gene in self.chromosome])}'
 
     def compute_fitness(self) -> float:
         return self.chromosome.chromosome_fitness()
@@ -56,6 +61,22 @@ class Knapsack_Individual(Individual):
             return new_individual
         else:
             return self
+
+    def trim_chromosome(self):
+        (max_weight, items) = Knapsack_World.problem
+        chromosome = self.chromosome
+        self.total_weight = sum([chromosome[i] * items[i].weight for i in range(len(chromosome))])
+        if self.total_weight <= max_weight:
+            return
+        selected_indices = [i for i in range(len(chromosome)) if chromosome[i]]
+        chromo_list = list(chromosome)
+        shuffle(selected_indices)
+        for i in selected_indices:
+            self.total_weight -= items[i].weight
+            chromo_list[i] = 0
+            if self.total_weight <= max_weight:
+                break
+        self.chromosome = Knapsack_Chromosome(chromo_list)
 
 
 class Knapsack_World(GA_World):
@@ -75,21 +96,36 @@ class Knapsack_World(GA_World):
     def gen_individual(self):
         # A Chromosome has as many positions as the problem has items.
         chromosome_list: List = [choice([0, 1]) for _ in range(len(Knapsack_World.problem[1]))]
-        individual = GA_World.individual_class(GA_World.chromosome_class(chromosome_list))
+        chromosome = Knapsack_Chromosome(chromosome_list)
+        individual = Knapsack_Individual(chromosome)
         return individual
 
+    def set_maximum_fitness_target(self, items, max_weight):
+        total_value = 0
+        total_weight = 0
+        for item in items:
+            total_value += item.value
+            total_weight += item.weight
+            if total_weight >= max_weight:
+                break
+        avg_density = total_value / total_weight
+        GA_World.fitness_target = floor(avg_density * max_weight)
+
     def set_results(self):
+        print('GA_World.fitness_target 2', GA_World.fitness_target)
         super().set_results()
-        print(f"Pop (fitness<-selection):  {',  '.join([str(ind) for ind in self.population])}")
-        print(f'  Best (fitness<-selection):  {self.best_ind}')
+        print(f"{self.generations}) Pop (value/weight<-selection):  {',  '.join([str(ind) for ind in self.population])}")
+        print(f'     Best (value/weight<-selection):  {self.best_ind}')
 
     def setup(self):
         GA_World.individual_class = Knapsack_Individual
         GA_World.chromosome_class = Knapsack_Chromosome
         problem_name = SimEngine.gui_get('Problem')
         Knapsack_World.problem = Knapsack_World.problems[problem_name]
-        (weight, items) = Knapsack_World.problem
-        print(f'\nNew Problem: (max weight: {weight}, items (value, weight)): '
+        (max_weight, items) = Knapsack_World.problem
+        self.set_maximum_fitness_target(items, max_weight)
+        SimEngine.gui_set('fitness_target', value=f'{GA_World.fitness_target}')
+        print(f'\nNew Problem (max weight: {max_weight}, items(value, weight)): '
               f'{", ".join([str(item) for item in items])}')
         super().setup()
 
@@ -102,14 +138,13 @@ knapsack_gui_left_upper = gui_left_upper + [
      sg.Slider(key='invert selection', range=(0, 100), default_value=25, orientation='horizontal', size=(10, 20))
      ],
 
-    [sg.Text('fitness target', pad=(None, (20, 0))),
-     sg.Slider(key='fitness_target', range=(0, 100), default_value=15, orientation='horizontal',
-               pad=((10, 0), (20, 0)), enable_events=True)
-     ],
-
     [sg.Text('Problem selection', pad=(None, (20, 0))),
      sg.Combo(key='Problem', default_value='Problem 1', pad=((10, 0), (20, 0)),
               values=['Problem 1'])
+     ],
+
+    [sg.Text('Fitness target:', pad=(None, (10, 0))),
+     sg.Text('      ', key='fitness_target', pad=(None, (10, 0)))
      ],
 
     ]
